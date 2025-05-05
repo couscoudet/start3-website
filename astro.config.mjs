401e1b36e485cdf1e929ce0ae9be293c26396d0c
couscoudet <ysoultane@gmail.com>
2025-05-05 10:42:15 +0200

    optimisations SEO et 404

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
+  integrations: [sitemap()],
   output: "static",
   site: "https://start3.tech-talent.fr", // Spécifie l'URL complète de votre site
   base: "/", // Préfixe pour les chemins relatifs (si votre app n'est pas à la racine)
